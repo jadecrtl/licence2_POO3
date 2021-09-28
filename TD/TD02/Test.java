3819c4b659f2129cd951c8fc8f48a5c67509048a
jadecrtl <jadecortial@gmail.com>
2021-09-28 11:08:05 +0200

    Exo 2 fini

---
+++ TD/TD02/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main (String[] args) {
+        A u = new A(0);
+        A v = new A(0);
+        u.g();
+        System.out.println("u: a=" + u.a + "; b=" + u.b);
+        v.g();
+        System.out.println("v: a=" + v.a + "; b=" + v.b);
+        u.g();
+        System.out.println("u: a=" + u.a + "; b=" + u.b);
+
+    }
+}
