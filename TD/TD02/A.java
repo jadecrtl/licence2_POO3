5d7d16f14a9d1958fa9d90c7047586fcd71426e2
jadecrtl <jadecortial@gmail.com>
2021-09-28 10:58:40 +0200

    Creation du rep TD01 et du TD02

---
+++ TD/TD02/A.java
@@ -0,0 +1,13 @@
+class A {
+    public static int a = 3;
+    public int b;
+
+    public A(int c) {
+        this.b = c;
+    }
+
+    public void g() {
+        a = a+1;
+        b = b+1;
+    }
+}
