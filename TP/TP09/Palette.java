c64ea4da01296cc4e4aaedeb360e5e68b30040e3
jadecrtl <jadecortial@gmail.com>
2021-11-25 15:37:39 +0100

    exo 1 et 2 fini du TP09 de poo

---
+++ TP/TP09/Palette.java
@@ -0,0 +1,14 @@
+public class Palette {
+    private Vue view;
+
+    public Palette() {
+        view = new Vue();
+        view.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Palette();
+    }
+
+
+}
---
+++ TP/TP09/Vue.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+public class Vue extends JFrame{
+    public Vue() {
+        setTitle("Palette");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+}
